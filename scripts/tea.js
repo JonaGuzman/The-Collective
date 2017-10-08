58f5deb9cd9fba8a9275090a00a7a1a04ec27d9e
Jonathan Guzman <guzman.jonathan.r@gmail.com>
2017-10-08 00:46:01 -0400

    updated json for tea pages

---
+++ scripts/tea.js
@@ -0,0 +1,24 @@
+
+function teaA1(){  
+    return '{ "name":"Tea One", "type":"Type 1", "inline":"#inline-content1", "content": "Consequat ea Investigationes in enim congue. Option velit volutpat quod blandit ex. Congue parum praesent aliquam nam clari. Qui praesent quam sollemnes id vulputate. In imperdiet diam at sequitur et. Minim delenit in dolor dolore typi.", "ratings":[40, 80, 65, 95]}';
+}
+
+function teaA2(){  
+    return '{ "name":"Tea Two", "type":"Type 1", "inline":"#inline-content2", "content": "Content2 ea Investigationes in enim congue. Option velit volutpat quod blandit ex. Congue parum praesent aliquam nam clari. Qui praesent quam sollemnes id vulputate. In imperdiet diam at sequitur et. Minim delenit in dolor dolore typi.", "ratings":[10, 90, 50, 100]}';
+}
+
+function teaA3(){  
+    return '{ "name":"Tea Three", "type":"Type 1", "inline":"#inline-content3", "content": "Content3 ea Investigationes in enim congue. Option velit volutpat quod blandit ex. Congue parum praesent aliquam nam clari. Qui praesent quam sollemnes id vulputate. In imperdiet diam at sequitur et. Minim delenit in dolor dolore typi.", "ratings":[40, 60, 80, 97]}';
+}
+
+function teaB1(){  
+    return '{ "name":"Tea Four", "type":"Type 2", "inline":"#inline-content4", "content": "Consequat ea Investigationes in enim congue. Option velit volutpat quod blandit ex. Congue parum praesent aliquam nam clari. Qui praesent quam sollemnes id vulputate. In imperdiet diam at sequitur et. Minim delenit in dolor dolore typi.", "ratings":[40, 80, 65, 95]}';
+}
+
+function teaB2(){  
+    return '{ "name":"Tea Five", "type":"Type 2", "inline":"#inline-content5", "content": "Content2 ea Investigationes in enim congue. Option velit volutpat quod blandit ex. Congue parum praesent aliquam nam clari. Qui praesent quam sollemnes id vulputate. In imperdiet diam at sequitur et. Minim delenit in dolor dolore typi.", "ratings":[10, 90, 50, 100]}';
+}
+
+function teaB3(){  
+    return '{ "name":"Tea Six", "type":"Type 2", "inline":"#inline-content6", "content": "Content3 ea Investigationes in enim congue. Option velit volutpat quod blandit ex. Congue parum praesent aliquam nam clari. Qui praesent quam sollemnes id vulputate. In imperdiet diam at sequitur et. Minim delenit in dolor dolore typi.", "ratings":[40, 60, 80, 97]}';
+}
