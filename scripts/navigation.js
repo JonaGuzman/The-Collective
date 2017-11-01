9ecba08e85e92e6ac8fddfed0dc22a286c51ae75
Jonathan Guzman <guzman.jonathan.r@gmail.com>
2017-11-01 06:47:19 -0400

    tried using ms instead of fast or  slow

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -81,7 +81,7 @@ function openNav() {
     wdth = '380px';
 
     
-    $(".nav-items").animate({ opacity: 1}).fadeIn("slow");
+    $(".nav-items").animate({ opacity: 1}, 300).fadeIn();
     document.getElementById(sidenav).style.width = wdth;
 }
 
