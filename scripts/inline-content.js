7ec0c6ae79b0f051aa2a7c9afc78e451332e0b18
Jonathan Guzman <guzman.jonathan.r@gmail.com>
2017-11-08 21:31:01 -0500

    added one word description for mobile view on tea page

--- scripts/inline-content.js
+++ scripts/inline-content.js
@@ -25,29 +25,52 @@ function setContentFlower(flowerName, type, inlineId, desc, catVal) {
 }
 
 function setContentTea(teaName, type, inlineId, desc, catVal) {
+     
+    if (Modernizr.mq('(max-width: 425px)')) {
+         let mobileDesc = ['low', 'med', 'high'];
+         
+         var inlineContent = "<div id=tea-name><h3>" + teaName + "</h3><h4>" + type + "</h4></div>" +
+                                "<div id=tea-info><ul>" +
+                                    "<li style=height:230px><img src=images/tea.jpg /></li>" +
+                                    "<li><div class=mobile-ratings><ul>" +
+                                    "<li>" + "<p>" + mobileDesc[0] + "  </p>" + "</li>" +
+                                    "<li>" + "<p>" + mobileDesc[1] + "  <span class='glyphicon glyphicon-ok'></span>" + "</p>" + "</li>" +
+                                    "<li>" + "<p>" + mobileDesc[2] + "  </p>" + "</li>" +            
+                                "</ul>" + 
+                                "<ul>" +
+                                    "<li>" + "<p>" + mobileDesc[0] + "  </p>" + "</li>" +
+                                    "<li>" + "<p>" + mobileDesc[1] + "  </p>" + "</li>" +
+                                    "<li>" + "<p>" + mobileDesc[2] + "  <span class='glyphicon glyphicon-ok'></span>" +"</p>" + "</li>" +            
+                                "</ul>" +
+                                "<ul>" +
+                                    "<li>" + "<p>" + mobileDesc[0] + "  <span class='glyphicon glyphicon-ok'></span>" +"</p>" + "</li>" +
+                                    "<li>" + "<p>" + mobileDesc[1] + "  </p>" + "</li>" +
+                                    "<li>" + "<p>" + mobileDesc[2] + "  </p>" +"</p>" + "</li>" +            
+                                "</ul>" + "</div></div>";
+    }
     
-    var catValues = catVal;
-    
-    // TODO: use modernizr to mq the size of the screen return list of categories split into 3 boxes with 3-4 desc each with a checkmark attribute
-    var progressBar1 = "<li><ol><li style=width:460px;height:21px><div class=progress>" +
-                        "<div class='progress-bar progress-bar-success' role=progressbar aria-valuenow=";
-    var progressBar2 = "aria-valuemin=0 aria-valuemax=100 style=width:";
-    
-    var inlineContent = "<div id=tea-name><h3>" + teaName + "</h3><h4>" + type + "</h4></div>" +
-                            "<div id=tea-info><ul>" +
-                                "<li style=height:230px><img src=images/tea.jpg /></li>" +
-                                "<li><p>" + desc + "</p></li>" +
-                                
-                                "<li><div class=ratings><ul>" +
-                                progressBar1 + catValues[0] +
-                                progressBar2 + catValues[0] + "%>Category 1</div></div></li></ol></li>" +            
-                                progressBar1 + catValues[1] +
-                                progressBar2 + catValues[1] + "%>Category 2</div></div></li></ol></li>" +
-                                progressBar1 + catValues[2] +
-                                progressBar2 + catValues[2] + "%>Category 3</div></div></li></ol></li>" +
-                                progressBar1 + catValues[3] +
-                                progressBar2 + catValues[3] + "%>Category 4</div></div></li></ol></li>" +
-                                "</ul></div>";
+    else {
+        let catValues = catVal;
+        let progressBar1 = "<li><ol><li style=width:460px;height:21px><div class=progress>" +
+                            "<div class='progress-bar progress-bar-success' role=progressbar aria-valuenow=";
+        let progressBar2 = "aria-valuemin=0 aria-valuemax=100 style=width:";
+
+        var inlineContent = "<div id=tea-name><h3>" + teaName + "</h3><h4>" + type + "</h4></div>" +
+                                "<div id=tea-info><ul>" +
+                                    "<li style=height:230px><img src=images/tea.jpg /></li>" +
+                                    "<li><p>" + desc + "</p></li>" +
+
+                                    "<li><div class=ratings><ul>" +
+                                    progressBar1 + catValues[0] +
+                                    progressBar2 + catValues[0] + "%>Category 1</div></div></li></ol></li>" +            
+                                    progressBar1 + catValues[1] +
+                                    progressBar2 + catValues[1] + "%>Category 2</div></div></li></ol></li>" +
+                                    progressBar1 + catValues[2] +
+                                    progressBar2 + catValues[2] + "%>Category 3</div></div></li></ol></li>" +
+                                    progressBar1 + catValues[3] +
+                                    progressBar2 + catValues[3] + "%>Category 4</div></div></li></ol></li>" +
+                                    "</ul></div>";
+    }
     
     $(inlineId).append(inlineContent);
 }
